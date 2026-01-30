5a4ddfd4967e2dedcebdb67ea1442b8838a68aac
Max Ng <maxngasuedu@gmail.com>
2026-01-30 18:30:24 +0000

    Have we Update next.config.ts in your repo Change it to: typescriptimpor

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,11 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+  trailingSlash: true,
+}
 
-export default nextConfig;
+export default nextConfig
